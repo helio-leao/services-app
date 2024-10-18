fa75bca4564a2457a79a55c2edcf9848b0f70959
Helio Leao <hmtleao@hotmail.com>
2024-10-18 17:39:19 -0300

    add more fieds to personal data screen

--- src/app/(tabs)/profile/signup/personal-data/index.tsx
+++ src/app/(tabs)/profile/signup/personal-data/index.tsx
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   View,
   TextInput,
+  ScrollView,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
@@ -15,57 +16,84 @@ export default function PersonalData() {
   const params = useLocalSearchParams();
   const [name, setName] = useState("");
   const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [numero, setNumero] = useState("");
+  const [complemento, setComplemento] = useState("");
   const [gender, setGender] = useState("");
 
   return (
     <SafeAreaView style={styles.screenContainer}>
-      {/* INPUTS SECTION */}
-      <View style={styles.textContainer}>
-        <Text style={[styles.title, { marginBottom: 20 }]}>
-          Informe seus dados pessoais
-        </Text>
-        <Text style={styles.title}>Nome</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Nome Completo"
-          onChangeText={setName}
-          value={name}
-        />
-        <Text style={styles.title}>CEP</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="99999-999"
-          onChangeText={setCep}
-          value={cep}
-        />
-        <Text style={styles.title}>Sexo</Text>
+      <ScrollView>
+        {/* INPUTS SECTION */}
+        <View style={styles.inputsContainer}>
+          <Text style={[styles.title, { marginBottom: 20 }]}>
+            Informe seus dados pessoais
+          </Text>
 
-        <View style={{ borderWidth: 1, borderRadius: 8 }}>
-          <Picker
-            selectedValue={gender}
-            onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
-          >
-            <Picker.Item label="Masculino" value="MASCULINO" />
-            <Picker.Item label="Feminino" value="FEMININO" />
-            <Picker.Item label="Outros" value="OUTROS" />
-          </Picker>
+          <Text style={styles.title}>Nome</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nome Completo"
+            onChangeText={setName}
+            value={name}
+          />
+
+          <Text style={styles.title}>CEP</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="99999-999"
+            onChangeText={setCep}
+            value={cep}
+          />
+          <Text style={styles.title}>Endereço</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setEndereco}
+            value={endereco}
+          />
+
+          <Text style={styles.title}>Número</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setNumero}
+            value={numero}
+          />
+
+          <Text style={styles.title}>Complemento</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setComplemento}
+            value={complemento}
+          />
+
+          <Text style={styles.title}>Sexo</Text>
+          <View style={{ borderWidth: 1, borderRadius: 8 }}>
+            <Picker
+              selectedValue={gender}
+              onValueChange={(itemValue, _itemIndex) => setGender(itemValue)}
+            >
+              <Picker.Item label="Masculino" value="MASCULINO" />
+              <Picker.Item label="Feminino" value="FEMININO" />
+              <Picker.Item label="Outros" value="OUTROS" />
+            </Picker>
+          </View>
         </View>
-      </View>
 
-      {/* BUTTONS SECTION */}
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity
-          style={styles.button}
-          // onPress={() =>
-          //   router.push({
-          //     pathname: "/profile/signup/service-subcategory",
-          //     params: { ...params, selectedSubcategory },
-          //   })
-          // }
-        >
-          <Text style={styles.buttonText}>Continuar</Text>
-        </TouchableOpacity>
-      </View>
+        {/* BUTTONS SECTION */}
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            // onPress={() =>
+            //   router.push({
+            //     pathname: "/profile/signup/service-subcategory",
+            //     params: { ...params, selectedSubcategory },
+            //   })
+            // }
+          >
+            <Text style={styles.buttonText}>Continuar</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -75,9 +103,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "space-between",
-    paddingVertical: 20,
   },
-  textContainer: {
+  inputsContainer: {
+    paddingVertical: 20,
     gap: 10,
     width: 300,
     alignSelf: "center",
@@ -96,6 +124,7 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     gap: 10,
+    marginBottom: 20,
   },
   button: {
     backgroundColor: "#000",
