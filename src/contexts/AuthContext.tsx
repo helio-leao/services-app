939151049d08020fcd36cacaeff8d1836dba7e04
Helio Leao <hmtleao@hotmail.com>
2024-10-21 21:55:05 -0300

    add auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useContext, ReactNode } from "react";
+import User from "../types/User";
+
+interface AuthContextType {
+  user: User | null;
+  login: (userData: User) => void;
+  logout: () => void;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (userData: User) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
---
+++ src/types/User.ts
@@ -0,0 +1,16 @@
+export default interface User {
+  _id: string;
+  cellphone: string;
+  email: string;
+  serviceCategory: string;
+  serviceSubcategory: string;
+  name: string;
+  cep: string;
+  address: string;
+  number: string;
+  complement: string;
+  gender: string;
+  photoUrl: string;
+  serviceTitle: string;
+  serviceDescription: string;
+}
