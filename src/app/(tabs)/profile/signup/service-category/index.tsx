3219dac818c5e884be2fe3c5044bca5d091bf592
Helio Leao <hmtleao@hotmail.com>
2024-10-24 14:49:58 -0300

    add api integration to service category screen

--- src/app/(tabs)/profile/signup/service-category/index.tsx
+++ src/app/(tabs)/profile/signup/service-category/index.tsx
@@ -1,91 +1,113 @@
 import { Image } from "expo-image";
 import { router, useLocalSearchParams } from "expo-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   StyleSheet,
   SafeAreaView,
   Text,
   TouchableOpacity,
   View,
+  ScrollView,
+  ActivityIndicator,
+  Alert,
 } from "react-native";
+import axios from "axios";
+import ServiceCategory from "@/src/types/ServiceCategory";
 
-const categories = [
-  {
-    name: "Assistência Técnica",
-    picture:
-      "https://s3-alpha-sig.figma.com/img/9d21/5142/c282a2746eb1283713b44afd414e1fd2?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KLsoKCb37iiFb2qnzzjo3~FRLMz1B7-vg7JI85dYx0pcuHwAbYT3gDEEgCAwvuZ3etBVN0SoqVnvmnn3NZXJHBxWsEki11w77E0Py2dEiloCkv8n4waMxlTuE0ty4Jla0n2Hp39QyXAweK-UhyoADV8zBRzujlj372fAPXTL5D7l3X2oBFzddIvNxG2GJTfEXY2Ic906RUsykIcL2IiLahplBklnlbLABHHmf9OF4EXGZFjmKWwFzSVO4FuyBPmP8sHO-CnYTBw4EkZdGkSqfytqbAFQf9fiwfliT3tEorInftAKiR5OLgkb568QGE4Jpi3lS2rlms66dvtAgLyLKQ__",
-  },
-  {
-    name: "Domésticos",
-    picture:
-      "https://s3-alpha-sig.figma.com/img/4e81/5c85/33210768d97194f03b29027427817438?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YKz7NTO7j2HDwBVHjuSKaj28rV~UkPtW09Tm-7jtGjRiCjZjsBunt1A4HBL5Jj13TGaSho62uI9AODbEd2VZL1hCDJVN0uX3dq0X9Ol4MUxgHO~NSOuxMzgD-KkhPr0Wafj4RCD8mHorNJQvbCBmhUcUpCPS7cufcM9YRc4s6rrHldHb~o~X2xMjGxB101HUKbVYRHkT-8rpOuczgGj80srxNtxZNI4zWzq3h55YInLkNpRnpPFj7eddP0j2y-3cNRAJ3LM8mCr8vL5w8cCDJJBD3j-uOOob~EqHBYl4OtpWn8rby8NVnXdQVJTcSAkXdUIXufjFksu29uKFeMl67g__",
-  },
-  {
-    name: "Reparos",
-    picture:
-      "https://s3-alpha-sig.figma.com/img/07d8/4682/5a49cbcb202f55897954e67a7cad8716?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JXiaUU1QOFoRLoU1ytPGXS--77KL1fIkt8zCBR~7kAasaSFE0FfdHfvLDGqTMZfgeO4W2MMwDGp6Bdg~GYlGacU9gy-fVBPFiqstAp8TOfvP5ty~3XOz8Bz2yYX5o-TAI59iOm1GFUR3EDiooYlRM8dOBOE96aKhWwl7OrEgsMR0ldrp4-43KsOUK7wMvu0cZULbsjyUJt2-VqdTl4tLAMracsqcZqtwbyrJry6QJtXk~5YH70pzkk5ni8YSrIeAq5H9xppjoBS6ALKFO2s9LLMgs-lwByoL~fXT3u-PAGZU2fa9eNqjMu1U4Wmjio8HUMO7X~rRFtJ~FqlVa25imA__",
-  },
-  {
-    name: "Aulas",
-    picture:
-      "https://s3-alpha-sig.figma.com/img/860f/a6eb/da444b507ed4d835220eef7eaeb6f61f?Expires=1729468800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FpmEY4Mlow~zlMbUjkokqumN8YIA4d6SIrlWXVbE8gesOsU868U2vs2mA0UFVJR3VIaxArGKqasQ3gGVeRmf0CiX8m-5UXoUAndp7FQLqmB9TnERytNKVoQO4dn8NTFIyuk~ZxIjd--rjFiHM7UwcVnz9WcE4BL4JnOs9Rnwt~2yOWUGBfq-x-MQjiAXIoGOFhHu4cAqY-h7MSSwQHx23rVTWt7GE9f~d4UUJV0PStVJFe6zFmm18MOERGj2UGEtZ0V1ztyqYiMRwPyyZKaRXW-LrPCcUOCYhmfvhMdpaRZev4QzBnU5BXiQTZPRhWRYg2LROi-9TRGNTAmDsacSUQ__",
-  },
-];
-
-export default function ServiceCategory() {
+export default function ServiceCategoryScreen() {
   const params = useLocalSearchParams();
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [categories, setCategories] = useState<ServiceCategory[]>([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState("");
+
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const { data } = await axios(
+          `${process.env.EXPO_PUBLIC_API_URL}/serviceCategories`
+        );
+        setCategories(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        Alert.alert("Atenção", "Não foi possível obter a lista de categorias.");
+      }
+    }
+    fetchCategories();
+  }, []);
 
   return (
     <SafeAreaView style={styles.screenContainer}>
-      {/* TEXT AND SELECT SECTION */}
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>
-          Selecione a categoria dos serviços que você vai realizar
-        </Text>
+      <ScrollView
+        contentContainerStyle={{
+          minHeight: "100%",
+          justifyContent: "space-between",
+          paddingVertical: 20,
+        }}
+      >
+        {/* SELECT SECTION */}
+        <View style={styles.selectContainer}>
+          <Text style={styles.title}>
+            Selecione a categoria dos serviços que você vai realizar
+          </Text>
 
-        <View style={{ gap: 8 }}>
-          {categories.map((option) => (
-            <TouchableOpacity
-              key={option.name}
-              style={[
-                styles.selectButton,
-                {
-                  backgroundColor:
-                    selectedCategory === option.name ? "#aaf" : "#ddd",
-                },
-              ]}
-              onPress={() =>
-                setSelectedCategory((prev) =>
-                  prev === option.name ? "" : option.name
-                )
-              }
+          {isLoading ? (
+            <View
+              style={{
+                flexGrow: 1,
+                justifyContent: "center",
+              }}
             >
-              <Image source={option.picture} style={styles.selectImage} />
-              <Text style={styles.title}>{option.name}</Text>
-            </TouchableOpacity>
-          ))}
+              <ActivityIndicator size={"large"} />
+            </View>
+          ) : (
+            <View style={{ gap: 8, marginVertical: 10 }}>
+              {categories.map((option) => (
+                <TouchableOpacity
+                  key={option._id}
+                  style={[
+                    styles.selectButton,
+                    {
+                      backgroundColor:
+                        selectedCategoryId === option._id ? "#aaf" : "#ddd",
+                    },
+                  ]}
+                  onPress={() =>
+                    setSelectedCategoryId((prev) =>
+                      prev === option._id ? "" : option._id
+                    )
+                  }
+                >
+                  <Image
+                    source={option.pictureUrl}
+                    style={styles.selectImage}
+                  />
+                  <Text style={styles.title}>{option.name}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          )}
         </View>
-      </View>
 
-      {/* BUTTONS SECTION */}
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity
-          style={[
-            styles.button,
-            { backgroundColor: !selectedCategory ? "#888" : "#000" },
-          ]}
-          disabled={!selectedCategory}
-          onPress={() =>
-            router.push({
-              pathname: "/profile/signup/service-subcategory",
-              params: { ...params, selectedCategory },
-            })
-          }
-        >
-          <Text style={styles.buttonText}>Continuar</Text>
-        </TouchableOpacity>
-      </View>
+        {/* BUTTONS SECTION */}
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity
+            style={[
+              styles.button,
+              { backgroundColor: !selectedCategoryId ? "#888" : "#000" },
+            ]}
+            disabled={!selectedCategoryId}
+            onPress={() =>
+              router.push({
+                pathname: "/profile/signup/service-subcategory",
+                params: { ...params, selectedCategoryId },
+              })
+            }
+          >
+            <Text style={styles.buttonText}>Continuar</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -94,11 +116,9 @@ const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "space-between",
-    paddingVertical: 20,
   },
-  textContainer: {
-    gap: 10,
+  selectContainer: {
+    flex: 1,
     width: 300,
     alignSelf: "center",
   },
---
+++ src/types/ServiceSubcategory.ts
@@ -0,0 +1,5 @@
+export default interface ServiceSubcategory {
+  _id: string;
+  name: string;
+  // serviceCategory
+}
