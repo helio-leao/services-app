a7d5d4294dcbc1db07c48cf18dd26a9b1ab4dec8
Helio Leao <hmtleao@hotmail.com>
2024-11-13 12:25:06 -0300

    add status bar style

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Slot } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
+import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
   return (
     <AuthProvider>
+      <StatusBar style="dark" />
       <Slot />
     </AuthProvider>
   );
