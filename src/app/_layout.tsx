337d0482ae064a2ddfb9118ccdd3d868b85f3554
Helio Leao <hmtleao@hotmail.com>
2024-11-13 14:03:24 -0300

    remove non working status bar style

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,9 @@
 import { Slot } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
-import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
   return (
     <AuthProvider>
-      <StatusBar style="dark" />
       <Slot />
     </AuthProvider>
   );
