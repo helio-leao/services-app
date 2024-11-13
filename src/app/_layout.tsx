891c5d53f4087e0c2557d2841557aa026ca39a73
Helio Leao <hmtleao@hotmail.com>
2024-11-13 15:48:10 -0300

    add working status bar style that bypasses expo bug

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,13 @@
 import { Slot } from "expo-router";
 import { AuthProvider } from "../contexts/AuthContext";
+import { setStatusBarStyle } from "expo-status-bar";
+import { useEffect } from "react";
 
 export default function Layout() {
+  useEffect(() => {
+    setStatusBarStyle("dark");
+  }, []);
+
   return (
     <AuthProvider>
       <Slot />
