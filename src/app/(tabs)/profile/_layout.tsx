d9bac468a10c67bf94ea4173da4d4ff4d9aa22c1
Helio Leao <hmtleao@hotmail.com>
2024-11-24 10:41:36 -0300

    style navigation header

--- src/app/(tabs)/profile/_layout.tsx
+++ src/app/(tabs)/profile/_layout.tsx
@@ -1,8 +1,32 @@
-import { Stack } from "expo-router";
+import { colors } from "@/src/constants/colors";
+import { AntDesign } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import { router, Stack } from "expo-router";
 
+// TODO: stack screenOptions is same in all stacks layout. centralize it
 export default function Layout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerTitle: "",
+        headerLeft: ({ canGoBack }) =>
+          canGoBack ? (
+            <TouchableOpacity
+              onPress={() => {
+                router.back();
+              }}
+              style={{
+                marginHorizontal: 10,
+                padding: 4,
+                borderRadius: 4,
+                backgroundColor: colors.primary,
+              }}
+            >
+              <AntDesign name="arrowleft" size={24} color={colors.background} />
+            </TouchableOpacity>
+          ) : null,
+      }}
+    >
       <Stack.Screen name="index" />
       <Stack.Screen name="home" options={{ title: "Perfil" }} />
       <Stack.Screen name="disclaimer" options={{ title: "Signup" }} />
